3c27c6c1d85e9afc6729f84b7da7630943f844fd
Maheep Brar <mbrar@purdue.edu>
2019-09-14 15:42:11 -0400

    Created firebase directory and added functionality for cloud firestore database

---
+++ app/src/main/java/com/example/helloworldbikefinder/firebase/UploadData.java
@@ -0,0 +1,31 @@
+package com.example.helloworldbikefinder.firebase;
+
+public class UploadData {
+    private String latitude;
+    private String longitude;
+    private String key;
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
