d9fc5cdc7123f129745006f272e7815b02195ce3
Maheep Brar <mbrar@purdue.edu>
2019-09-15 01:25:16 -0400

    Added location to upload and connected upload to uploadData

--- app/src/main/java/com/example/helloworldbikefinder/Upload.java
+++ app/src/main/java/com/example/helloworldbikefinder/Upload.java
@@ -8,10 +8,41 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+import com.example.helloworldbikefinder.firebase.UploadData;
 
 import java.nio.ByteBuffer;
 
 public class Upload extends AppCompatActivity {
+    private byte[] img;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private String latitude;
+    private String longitude;
 
     // ImageView imageView;
 
@@ -22,7 +53,7 @@ public class Upload extends AppCompatActivity {
 
 
         // The following follows https://www.youtube.com/watch?v=ondCeqlAwEI
-        Button cameraBtn = (Button)findViewById(R.id.cameraButton);
+        Button cameraBtn = (Button) findViewById(R.id.cameraButton);
         //imageView = (ImageView)findViewById(R.id.imageView); // For displaying image after
 
         System.out.println("1");
@@ -40,11 +71,62 @@ public class Upload extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
         //imageView.setImageBitmap(bitmap); //For displaying the image after
-        byte[] img = convertBitmaptoBytes(bitmap);
+        img = convertBitmaptoBytes(bitmap);
+        uploadData();
         System.out.println("3");
     }
+
+    public void uploadData() {
+        acquireLocation();
+        UploadData uploadData = new UploadData(img, latitude, longitude);
+        uploadData.upload();
+    }
+
+    public void acquireLocation() {
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = "" + location.getLatitude();
+                longitude = "" + location.getLongitude();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+
+
+            }
+        };
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 10);
+                return;
+            }
+            }
+        }
+
+
+
+
+
 
     public byte[] convertBitmaptoBytes(Bitmap bitmap) {
         ByteBuffer bb = ByteBuffer.allocate(bitmap.getByteCount());
