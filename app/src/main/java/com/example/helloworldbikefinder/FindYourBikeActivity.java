809efa9d9ce509a24a86a83f79cbd14f00a0b65a
Kaung Myat Zaw <kaungmyatzaw611@gmail.com>
2019-09-15 01:32:52 -0400

    Added image functionality to FindYourBikeActivity.java

--- app/src/main/java/com/example/helloworldbikefinder/FindYourBikeActivity.java
+++ app/src/main/java/com/example/helloworldbikefinder/FindYourBikeActivity.java
@@ -1,6 +1,8 @@
 package com.example.helloworldbikefinder;
 
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import com.example.helloworldbikefinder.firebase.AccessData;
@@ -12,17 +14,25 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Map;
 
+import io.grpc.internal.IoUtils;
+
 public class FindYourBikeActivity extends AppCompatActivity {
 
-    ListView bicycleListView;
-    String[] locations;
+    ScrollView bicycleScrollView;
+    byte[] img_byte_array;
+    Bitmap[] bitmapImages;
     ArrayList<Map> data;
     ArrayList<String> docIDs;
     ArrayList<BikeTab> bikeTabs;
@@ -32,13 +42,29 @@ public class FindYourBikeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_your_bike);
 
-        Resources res = getResources();
-        bicycleListView = (ListView) findViewById(R.id.bicycleListView);
-        locations = res.getStringArray(R.array.locations);
+        collectAllData();
+        createTabs();
 
-        bicycleListView.setAdapter(new ArrayAdapter<String>(this, R.layout.bicycle_list_view_detail, locations) {
+        for (int i = 0; i < data.size(); i++) {
+            String url = (String) data.get(i).get("downloadUrl");
+            try {
+                BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+                img_byte_array = IoUtils.toByteArray(in);
 
-        });
+                // now convert the img_byte_array to bitmap and display it on ImageView
+                Bitmap bmp = BitmapFactory.decodeByteArray(img_byte_array, 0, img_byte_array.length);
+                bitmapImages[i] = bmp;
+
+            } catch (IOException e) {
+                // do nothing here
+                // hope to God it fucking works and there are no exceptions
+            }
+        }
+
+        bicycleScrollView = (ScrollView) findViewById(R.id.bicycleScrollView);
+        ImageView imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView1.setImageBitmap(Bitmap.createScaledBitmap(bitmapImages[1], imageView1.getWidth(), imageView1.getHeight(), false));
+
     }
 
    public void collectAllData() {
